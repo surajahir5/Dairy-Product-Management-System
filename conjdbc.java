8b0c8d2efc47fab42c0255660da5e6d653b3c0df
Suraj Yadav <118503877+surajahir5@users.noreply.github.com>
2023-02-25 14:28:06 +0530

    Add files via upload

---
+++ conjdbc.java
@@ -0,0 +1,20 @@
+
+import java.sql.Connection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author suraj
+ */
+class conjdbc {
+
+    static Connection getConnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
